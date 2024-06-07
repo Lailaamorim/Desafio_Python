45ecbd7c48cfcddadf90a568129d1502931c0111
Laila Hamorym <lailaamorimsant@gmail.com>
2024-06-06 22:03:31 -0300

    Finalização da aula 14 e 15 'comentados' e finalização do MUNDO 2 EM PYTHON ;)

--- Aula015/Desafio071.py
+++ Aula015/Desafio071.py
@@ -4,11 +4,17 @@ o valor a ser sacado (número inteiro) a o programa vai informar quantas cédula
 
 OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 R41
 '''    
-total = int(input('digite um valor inteiro: '))
+total = int(input('Digite um valor inteiro: '))  # Solicita ao usuário que digite um valor inteiro
+
+# Loop for para iterar sobre os valores das cédulas
 for i in (50, 20, 10, 1):
-    tced = 0
+    tced = 0  # Inicializa o contador de cédulas para cada valor
+    
+    # Loop while para decompor o valor em cédulas do valor atual (i)
     while total >= i:
-        total -= i
-        tced += 1
+        total -= i  # Subtrai o valor da cédula atual do total
+        tced += 1  # Incrementa o contador de cédulas do valor atual
+    
+    # Se foram retiradas cédulas deste valor, exibe o número total de cédulas
     if tced != 0:
         print(f'Total de cédulas de R$ {i}: {tced}')
